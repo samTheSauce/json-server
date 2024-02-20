7aa54c7922c278837e5e7e00fe8374af6f906198
unknown <estherpuyang@gmail.com>
2024-02-20 16:36:30 +0800

    update

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const server = jsonServer.create();
 const router = jsonServer.router("db/db.json");
 const middlewares = jsonServer.defaults();
 
+
 server.use(middlewares);
 // Add this before server.use(router)
 server.use(
@@ -17,5 +18,4 @@ server.listen(3000, () => {
 	console.log("JSON Server is running");
 });
 
-// Export the Server API
-module.exports = server;
+
