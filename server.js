e6a1888c05165e1e7fb7a29bbbdb7ca8375ce3b3
unknown <estherpuyang@gmail.com>
2024-03-12 15:27:40 +0800

    updated

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
-// JSON Server module
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db/db.json");
-const middlewares = jsonServer.defaults();
+const jsonServer = require('json-server')
 
+const server = jsonServer.create()
 
-server.use(middlewares);
+// Uncomment to allow write operations
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db/db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
+
+// Comment out to allow write operations
+// const router = jsonServer.router('db/db.json')
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
 // Add this before server.use(router)
-server.use(
-	// Add custom route here if needed
-	jsonServer.rewriter({
-		"/api/*": "/$1",
-	})
-);
-server.use(router);
-server.listen(3000, () => {
-	console.log("JSON Server is running");
-});
+server.use(jsonServer.rewriter({
+	'/api/*': '/$1'
+}))
+server.use(router)
+server.listen(5000, () => {
+	console.log('JSON Server is running')
+})
 
-
+// Export the Server API
+module.exports = server
