4a8070304ada7f68059d6ef526c12b1deff07bd3
unknown <estherpuyang@gmail.com>
2024-03-12 14:37:45 +0800

    updated

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 // JSON Server module
 const jsonServer = require("json-server");
-const cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router("db/db.json");
 const middlewares = jsonServer.defaults();
 
-server.use(cors());
+
 server.use(middlewares);
 // Add this before server.use(router)
 server.use(
@@ -15,7 +14,7 @@ server.use(
 	})
 );
 server.use(router);
-server.listen(3001, () => {
+server.listen(3000, () => {
 	console.log("JSON Server is running");
 });
 
